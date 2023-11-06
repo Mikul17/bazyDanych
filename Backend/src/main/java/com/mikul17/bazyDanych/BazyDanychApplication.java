6e2480afc8a6c30b644254ac0e301953ce063508
Mikul17 <mikulski2016@gmail.com>
2023-11-06 12:29:52 +0100

    backend init

---
+++ Backend/src/main/java/com/mikul17/bazyDanych/BazyDanychApplication.java
@@ -0,0 +1,13 @@
+package com.mikul17.bazyDanych;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazyDanychApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazyDanychApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/mikul17/bazyDanych/BazyDanychApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikul17.bazyDanych;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BazyDanychApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
