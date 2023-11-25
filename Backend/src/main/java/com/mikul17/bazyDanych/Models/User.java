9a07d9288c8a450f9b7b41feef2895de3d576001
Mikul17 <mikulski2016@gmail.com>
2023-11-25 21:57:06 +0100

    added data validation

--- Backend/src/main/java/com/mikul17/bazyDanych/Models/User.java
+++ Backend/src/main/java/com/mikul17/bazyDanych/Models/User.java
@@ -1,6 +1,9 @@
 package com.mikul17.bazyDanych.Models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import java.sql.Timestamp;
 import java.time.LocalDate;
@@ -17,25 +20,42 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
     private Long Id;
-    @Column(nullable = false, columnDefinition = "varchar(30)")
+
+    @NotBlank(message = "First name is required")
+    @Size(max = 30, message = "First name cannot be longer than 30 characters")
     private String firstName;
-    @Column(nullable = false, columnDefinition = "varchar(30)")
+
+    @NotBlank(message = "Last name is required")
+    @Size(max = 30, message = "Last name cannot be longer than 30 characters")
     private String lastName;
-    @Column(nullable = false, columnDefinition = "varchar(45)")
+
+    @NotBlank(message = "Email is required")
+    @Size(max = 50, message = "Email cannot be longer than 50 characters")
+    @Email
     private String email;
-    @Column(nullable = false, columnDefinition = "varchar(25)")
+
+    @NotBlank(message = "Password is required")
+    @Size(max = 50, message = "Password cannot be longer than 50 characters")
     private String password;
-    @Column(nullable = false, columnDefinition = "varchar(15)")
+
+    @NotBlank(message = "Phone number is required")
+    @Size(min = 2, max = 15, message = "Phone number must be between 2 and 15 characters")
     private String phoneNumber;
+
     private LocalDate birthDate;
-    @Column(columnDefinition = "varchar(26)")
+
+    @Size(min = 26, max = 26, message = "Account number must be 26 characters long")
     private String accountNumber;
-    @Column(nullable = false, columnDefinition = "varchar(11)")
+
+    @Size(min = 11, max = 11, message = "SSN must be 11 characters long")
+    @NotBlank(message = "SSN is required")
     private String SSN;
-    @Column(nullable = false, columnDefinition = "varchar(5)")
+
     private String role;
+
     @Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     private Timestamp createdAt;
+
     @Column(columnDefinition = "DECIMAL(19,2)")
     private Double balance;
 
