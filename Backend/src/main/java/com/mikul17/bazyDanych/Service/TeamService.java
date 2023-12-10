89505edad09c164bce857a486bb76e7711b07b86
Mikul17 <mikulski2016@gmail.com>
2023-12-10 03:57:13 +0100

    fixed service and controller type returns

--- Backend/src/main/java/com/mikul17/bazyDanych/Service/TeamService.java
+++ Backend/src/main/java/com/mikul17/bazyDanych/Service/TeamService.java
@@ -6,12 +6,14 @@ import com.mikul17.bazyDanych.Repository.LeagueRepository;
 import com.mikul17.bazyDanych.Repository.TeamRepository;
 import com.mikul17.bazyDanych.Request.TeamRequest;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.service.spi.ServiceException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -24,7 +26,7 @@ public class TeamService {
     private final LeagueRepository leagueRepository;
     private final PlayerRepository playerRepository;
 
-    public ResponseEntity<?> createTeam(TeamRequest request) throws Exception {
+    public Team createTeam(TeamRequest request) {
         try{
             Team team = Team.builder()
                     .teamName(request.getTeamName())
@@ -38,14 +40,14 @@ public class TeamService {
                     .leaguePoints(0)
                     .build();
             log.info("Team created: {} in league : {}", team.getTeamName(), team.getLeague().getLeagueName());
-            return ResponseEntity.ok(teamRepository.save(team));
+            return teamRepository.save(team);
         }catch (Exception e){
-            log.error("Error while creating team: {}", e.getMessage());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+            throw new ServiceException("Error while adding team: "+e.getMessage());
         }
     }
-    public ResponseEntity<?> createTeamsFromList(List<TeamRequest> teams) {
+    public List<Team> createTeamsFromList(List<TeamRequest> teams) {
         try{
+            List<Team> teamList = new ArrayList<>();
             for (TeamRequest teamRequest : teams) {
                 Team team = Team.builder()
                         .teamName(teamRequest.getTeamName())
@@ -59,48 +61,52 @@ public class TeamService {
                         .leaguePoints(0)
                         .build();
                 teamRepository.save(team);
+                teamList.add(team);
                 log.info("Team created: {} in league : {}", team.getTeamName(), team.getLeague().getLeagueName());
             }
-            return ResponseEntity.ok(teams.size()+" teams created !");
+            return teamList;
         } catch (Exception e) {
-            log.error("Error while creating teams from list: {}", e.getMessage());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+            throw new ServiceException("Error while creating teams from list: "+e.getMessage());
         }
     }
-    public ResponseEntity<?> getTeamsByLeagueId(Long id) throws Exception {
+    public List<Team> getTeamsByLeagueId(Long id) throws Exception {
         try{
-            return ResponseEntity.ok(teamRepository.findAllByLeagueId(id));
+            return teamRepository.findAllByLeagueId(id);
         }catch (Exception e){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+            throw new ServiceException("Error while getting teams by league id", e);
         }
     }
-    public ResponseEntity<?> getTeamById(Long id) throws Exception {
+    public Team getTeamById(Long id) {
+        return teamRepository.findById(id).orElseThrow(()
+                    -> new ServiceException("Team not found"));
+    }
+    public List<Team> getAllTeams() {
         try{
-            return ResponseEntity.ok(teamRepository.findById(id).orElseThrow(()
-                    -> new Exception("Team not found")));
+            return teamRepository.findAll();
         }catch (Exception e){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+            throw new ServiceException("Error while getting teams", e);
         }
     }
-    public ResponseEntity<?> getAllTeams() {
-        return ResponseEntity.ok(teamRepository.findAll());
-    }
-    public ResponseEntity<?> deleteTeam(Long id) throws Exception {
+    public void deleteTeam(Long id) {
         try{
             teamRepository.findById(id).orElseThrow(()
-                    -> new Exception("Team not found"));
+                    -> new ServiceException("Team not found"));
             teamRepository.deleteById(id);
-            log.info("Team deleted: {}", id);
-            return ResponseEntity.ok("Team deleted");
         }catch (Exception e){
-            log.error("Error while deleting team: {}", e.getMessage());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+           throw new ServiceException("Error while deleting team: "+e.getMessage());
         }
     }
-    public ResponseEntity<?> resetTeamStats(Long id) throws Exception {
+    public void deleteAllTeams() {
+        try{
+            teamRepository.deleteAll();
+        }catch (Exception e){
+            throw new ServiceException("Error while deleting teams: "+e.getMessage());
+        }
+    }
+    public Team resetTeamStats(Long id) {
         try{
             Team team = teamRepository.findById(id).orElseThrow(()
-                    -> new Exception("Team not found"));
+                    -> new ServiceException("Team not found"));
             team.setWins(0);
             team.setDraws(0);
             team.setLoses(0);
@@ -108,46 +114,19 @@ public class TeamService {
             team.setGoalsConceded(0);
             team.setLeaguePoints(0);
             log.info("Team stats reset: {}", team.getTeamName());
-            return ResponseEntity.ok(teamRepository.save(team));
+            return teamRepository.save(team);
         }catch (Exception e){
-            log.error("Error while resetting team stats: {}", e.getMessage());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+           throw new ServiceException("Error while reseting team: "+e.getMessage());
         }
     }
-    public ResponseEntity<?> deleteAllTeams() {
-        try{
-            teamRepository.deleteAll();
-            log.info("All teams deleted");
-            return ResponseEntity.ok("All teams deleted");
-        }catch (Exception e){
-            log.error("Error while deleting all teams: {}", e.getMessage());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-        }
-    }
-    public ResponseEntity<?> deleteTeamByTeamName(String teamName) throws Exception {
+    public void deleteTeamByTeamName(String teamName) {
         try{
             Team team = teamRepository.findByTeamName(teamName).orElseThrow(()
-                    -> new Exception("Team not found"));
+                    -> new ServiceException("Team not found"));
             teamRepository.delete(team);
-            log.info("Team deleted: {}", teamName);
-            return ResponseEntity.ok("Team "+teamName+" deleted successfully");
         }catch (Exception e){
-            log.error("Error while deleting team: {}", e.getMessage());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+           throw new ServiceException("Error while deleting team by teamName: "+e.getMessage());
         }
     }
 
-    private ResponseEntity<?> updateTeamCaptain(Long id, Long playerId) throws Exception {
-        try{
-            Team team = teamRepository.findById(id).orElseThrow(()
-                    -> new Exception("Team not found"));
-            team.setCaptain(playerRepository.findById(playerId).orElseThrow(()
-                    -> new Exception("Player not found")));
-            log.info("New captain set: {} for team: {}", team.getCaptain().getLastName(), team.getTeamName());
-            return ResponseEntity.ok(teamRepository.save(team));
-        }catch (Exception e){
-            log.error("Error while setting new captain: {}", e.getMessage());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-        }
-    }
 }
