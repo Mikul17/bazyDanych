0733ab1cbf6f57321152292d7353691ba37e5640
Mikul17 <mikulski2016@gmail.com>
2023-12-10 04:21:59 +0100

    fixed service and controller type returns for player related classes

--- Backend/src/main/java/com/mikul17/bazyDanych/Controller/PlayerStatController.java
+++ Backend/src/main/java/com/mikul17/bazyDanych/Controller/PlayerStatController.java
@@ -2,6 +2,9 @@ package com.mikul17.bazyDanych.Controller;
 
 import com.mikul17.bazyDanych.Service.PlayerStatService;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.service.spi.ServiceException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,9 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class PlayerStatController {
 
     private final PlayerStatService playerStatService;
+    private final Logger logger = LoggerFactory.getLogger(PlayerStatController.class);
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getPlayerStatById(@PathVariable Long id){
-        return playerStatService.getPlayerStatById(id);
+       try{
+           return ResponseEntity.ok(playerStatService.getPlayerStatById(id));
+       }catch (ServiceException e){
+           logger.error(e.getMessage());
+           return ResponseEntity.badRequest().body(e.getMessage());
+       }catch (Exception e){
+           logger.error(e.getMessage());
+           return ResponseEntity.internalServerError().body(e.getMessage());
+       }
     }
 }
